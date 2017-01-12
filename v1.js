0c9b9bedf69dc5327f40347f4499e0de9bf0d650
robobee <igor.serpak@gmail.com>
2017-01-12 16:06:26 +0200

    Changes

--- v1.js
+++ v1.js
@@ -1,28 +1,28 @@
-// const nextCharForNumberString = str => {
-//   const trimmed = str.trim()
-//   const number = parseInt(trimmed)
-//   const nextNumber = number + 1
-//   return String.fromCharCode(nextNumber)
-// }
+const nextCharForNumberString = str => {
+  const trimmed = str.trim()
+  const number = parseInt(trimmed)
+  const nextNumber = number + 1
+  return String.fromCharCode(nextNumber)
+}
+
+// // const nextCharForNumberString = str =>
+// //   String.fromCharCode(parseInt(str.trim()) + 1)
+
+// // identity functor
+// const Box = x =>
+// ({
+//   map: f => Box(f(x)),
+//   inspect: f => `Box(${x})`,
+//   fold: f => f(x)
+// })
 
 // const nextCharForNumberString = str =>
-//   String.fromCharCode(parseInt(str.trim()) + 1)
-
-// identity functor
-const Box = x =>
-({
-  map: f => Box(f(x)),
-  inspect: f => `Box(${x})`,
-  fold: f => f(x)
-})
-
-const nextCharForNumberString = str =>
-  Box(str)
-  .map(s => s.trim())
-  .map(r => new Number(r))
-  .map(i => i + 1)
-  .map(i => String.fromCharCode(i))
-  .fold(c => c.toLowerCase())
+//   Box(str)
+//   .map(s => s.trim())
+//   .map(r => new Number(r))
+//   .map(i => i + 1)
+//   .fold(i => String.fromCharCode(i))
+//   // .fold(c => c.toLowerCase())
 
 const result = nextCharForNumberString('  64  ')
 
